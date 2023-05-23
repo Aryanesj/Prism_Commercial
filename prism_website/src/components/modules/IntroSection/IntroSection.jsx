fa6e1c10bc911348608c59ed5f7b02e15168e7f5
Artem <nevadaltd88@gmail.com>
2023-05-23 14:40:54 +0300

    Added IntroSection and changed some things

--- prism_website/src/components/modules/IntroSection/IntroSection.jsx
+++ prism_website/src/components/modules/IntroSection/IntroSection.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { ReactComponent as ArrowRight } from '../../../images/IntroSection/Icon.svg'
 import styles from './IntroSection.module.scss'
 
 const IntroSection = () => {
     return (
-        <div className={styles.header}>
-            <div className="container">
-                <h1>Intro Prism Section will be soon...</h1>
+        <div className={styles.intro}>
+            <div className={styles.intro__container}>
+                <h1>Prism</h1>
+                <p>Аналітика ризиків за 15 напрямками</p>
             </div>
+            <button className={styles.intro__btn}>
+                Напрямки та інструменти <br /> аналізу
+                <ArrowRight className={styles.intro__arrowRight}/>
+            </button>
         </div>
     )
 }
