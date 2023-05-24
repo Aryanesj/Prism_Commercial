0442a5e1af227d94e6af8793f2bab8f8e05cc4cd
Artem <nevadaltd88@gmail.com>
2023-05-24 20:48:33 +0300

    Added Footer Section

--- prism_website/src/components/modules/Footer/Footer.jsx
+++ prism_website/src/components/modules/Footer/Footer.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { menuData } from '../../data/MenuData'
+import { ReactComponent as MailIcon } from '../../../images/Footer/Mail.svg'
+import { ReactComponent as PhoneIcon } from '../../../images/Footer/Phone.svg'
+import PrismLogo from '../../../images/Footer/Logo.svg'
 import styles from './Footer.module.scss'
 
 const Footer = () => {
     return (
         <section className={styles.footer}>
             <div className={styles.footer__container}>
-                <h1>Footer will be soon</h1>
+                <h1 className={styles.footer__title}>Контакти</h1>
+                    <div className={styles.footer__mailBlock}>
+                        <MailIcon />
+                        <p className={styles.footer__mailText}>prism.ra2020@gmail.com</p>
+                    </div>
+                    <div className={styles.footer__phoneBlock}>
+                        <PhoneIcon></PhoneIcon>
+                        <p className={styles.footer__phoneText}>+38 063 244 83 85</p>
+                    </div>
+                    <div className={styles.footer__copyHeaderBlock}>
+                        <a href="/" className={styles.footer__logoLink}>
+                            <img src={PrismLogo} alt="Prism logo" />
+                        </a>
+                        <ul>
+                            {menuData.map(({ link, title }, index) => (
+                                <li key={index}>
+                                    <NavLink to={link} className={styles.footer__link} >
+                                        {title}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
             </div>
         </section>
     )
